b38dbbc2e27978dc360072ded5f2c563693490f6
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-08-26 10:25:48 +0300

    Add project files.

---
+++ OdataServiceSolution/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using OdataServiceSolution.Model;
+
+namespace OdataServiceSolution.Controllers
+{
+
+    [Route("api/[controller]")]
+    [EnableQuery]
+    public class CategoriesController : ODataController
+    {
+        private readonly List<Category> _categories = new List<Category>()
+        {
+            new Category(){ CategoryId = 1, CategoryName = "category1", Description="Category 1 description"},
+            new Category(){ CategoryId = 2, CategoryName = "category2", Description="Category 2 description"},
+            new Category(){ CategoryId = 3, CategoryName = "category3", Description="Category 3 description"},
+            new Category(){ CategoryId = 4, CategoryName = "category4", Description="Category 4 description"},
+            new Category(){ CategoryId = 5, CategoryName = "category5", Description="Category 5 description"},
+            new Category(){ CategoryId = 6, CategoryName = "category6", Description="Category 6 description"},
+        };
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> Get()
+        {
+            return _categories;
+        }
+
+    }
+}
---
+++ OdataServiceSolution/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using OdataServiceSolution.Controllers;
+using OdataServiceSolution.Model;
+
+namespace OdataServiceSolution
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+                options.RespectBrowserAcceptHeader = true;
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddOData();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            var builder = new ODataConventionModelBuilder(app.ApplicationServices);
+
+            builder.EntitySet<Category>(nameof(Category));
+
+            app.UseMvc(routeBuilder =>
+            {
+                // and this line to enable OData query option, for example $filter
+                routeBuilder.Select().Expand().Filter().OrderBy().MaxTop(null).Count();
+
+                routeBuilder.MapODataServiceRoute("ODataRoute", "odata", builder.GetEdmModel());
+
+                // uncomment the following line to Work-around for #1175 in beta1
+                routeBuilder.EnableDependencyInjection();
+            });
+        }
+    }
+}
